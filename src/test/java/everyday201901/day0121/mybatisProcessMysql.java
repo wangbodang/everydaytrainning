178015b2ba90f5ca4333e6e9aafabd5877756710
wangbodang <870719738@qq.com>
2019-01-21 16:49:13 +0800

    操作大量数据时...

--- src/test/java/everyday201901/day0121/mybatisProcessMysql.java
+++ src/test/java/everyday201901/day0121/mybatisProcessMysql.java
@@ -13,6 +13,9 @@ import java.io.InputStream;
 
 public class mybatisProcessMysql {
 
+    /***
+     * 测试下
+     */
     @Test
     public void insertManyRecords(){
         String resource = "mybatis-conf/mybatis-conf.xml";
@@ -53,6 +56,78 @@ public class mybatisProcessMysql {
                 System.out.println(" ==> 插入一条数据  其id为:"+insertEmp.getId());
             }
             mysql_SqlSession.commit();
+            mysql_SqlSession.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试下插入数万条记录需要的时间
+     */
+    @Test
+    public void insertOneMillionRecords(){
+        String resource = "mybatis-conf/mybatis-conf.xml";
+        try {
+
+            System.out.println("\n\n\n--->使用Mysql数据源");
+            //再获得一遍
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory mysql_sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream, "dev_mysql");
+
+            SqlSession mysql_SqlSession = mysql_sqlSessionFactory.openSession();
+            EmployeeMapper employeeMapper = mysql_SqlSession.getMapper(EmployeeMapper.class);
+
+            int counts = 30000;
+
+            long startTime = System.currentTimeMillis();
+            for(int i=0;i<counts;i++){
+                Employee insertEmp = new Employee();
+                insertEmp.setName("孙承宗");
+                insertEmp.setAge(39);
+                int insertRows = employeeMapper.insertSelective(insertEmp);
+                //mysql_SqlSession.commit();
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println("---> 插入"+counts+"条记录, 共用时:"+(endTime-startTime)/1000+"秒");
+            mysql_SqlSession.commit();
+            mysql_SqlSession.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    //测试下数据库删除记录需要的时间
+    //删除超加大于11021的记录
+    @Test
+    public void delManyRecords(){
+        String resource = "mybatis-conf/mybatis-conf.xml";
+        try {
+
+            System.out.println("\n\n\n--->使用Mysql数据源");
+            //再获得一遍
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory mysql_sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream, "dev_mysql");
+
+            SqlSession mysql_SqlSession = mysql_sqlSessionFactory.openSession();
+            EmployeeMapper employeeMapper = mysql_SqlSession.getMapper(EmployeeMapper.class);
+
+            long startTime = System.currentTimeMillis();
+
+            int maxId = employeeMapper.findMaxId();
+            int counts = 0;
+            while(maxId>11021){
+                int rows = employeeMapper.deleteByPrimaryKey(maxId);
+                counts++;
+                maxId = employeeMapper.findMaxId();
+            }
+            mysql_SqlSession.commit();
+
+            long endTime = System.currentTimeMillis();
+            System.out.println("---> 插入"+counts+"条记录, 共用时:"+(endTime-startTime)/1000+"秒");
+            mysql_SqlSession.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
