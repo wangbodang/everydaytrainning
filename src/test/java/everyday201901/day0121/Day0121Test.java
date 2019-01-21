1b0b828832de9b49cc71afcb3aaa853afda732fa
wangbodang <870719738@qq.com>
2019-01-21 10:35:37 +0800

    添加commons-lang包

--- src/test/java/everyday201901/day0121/Day0121Test.java
+++ src/test/java/everyday201901/day0121/Day0121Test.java
@@ -1,10 +1,25 @@
 package everyday201901.day0121;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 public class Day0121Test {
+
+
     @Test
     public void testDay0121(){
         System.out.println("--->输出内容!!!");
+    }
+
+    @Test
+    public void testStringUtils(){
+        String demo = "";
+        System.out.println("-->字符串长度为:"+demo.length());
+        if(StringUtils.isBlank(demo)){
+            System.out.println("--->是空格");
+        }else{
+            System.out.println("--->不是空");
+        }
+
     }
 }
