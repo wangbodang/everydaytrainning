3dfae57fd9de6bdc20b6b507a05a1592929913fd
wangbodang <870719738@qq.com>
2019-01-23 17:22:16 +0800

    BigDecimal 除法运算时的精度

--- src/test/java/everyday201901/day0123/BigDecimalTest.java
+++ src/test/java/everyday201901/day0123/BigDecimalTest.java
@@ -3,6 +3,7 @@ package everyday201901.day0123;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class BigDecimalTest {
 
@@ -14,5 +15,17 @@ public class BigDecimalTest {
             total = total.add(new BigDecimal(i));
         }
         System.out.println(total);
+    }
+
+    /**
+     * BigDecimal除法时精度的处理
+     */
+    @Test
+    public void testBigDecimalDivide(){
+        BigDecimal b1 = new BigDecimal("2323.34");
+        BigDecimal b2 = new BigDecimal("34.3");
+        System.out.println(b1.divide(b2, 2, BigDecimal.ROUND_HALF_UP));
+        BigDecimal b3 = b1.divide(b2, MathContext.DECIMAL32);
+        System.out.println(b3);
     }
 }
