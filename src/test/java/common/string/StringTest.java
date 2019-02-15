ae90d0c7a09878b48ce56083cab8ed014369fb30
wangbodang <870719738@qq.com>
2019-02-15 16:07:44 +0800

    分段字符串

--- src/test/java/common/string/StringTest.java
+++ src/test/java/common/string/StringTest.java
@@ -1,5 +1,6 @@
 package common.string;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 public class StringTest {
@@ -13,6 +14,18 @@ public class StringTest {
         String[] strArr = demo.split(",");
         for(String s:strArr){
             System.out.println(s);
+        }
+    }
+
+    @Test
+    public void testSubString(){
+        String skm = "035*17-+*43>7><<**6/56994<13<56>*59272*<-541-2-01-15/765976830<819<6>92-398342191350+8298240>*011363037193>6*106";
+        if(StringUtils.isNotBlank(skm)){
+            int lines = 4;
+            int rowCounts = skm.length()/4;
+            for(int i=0;i<lines;i++){
+                System.out.println(skm.substring(i*rowCounts, (i+1)*rowCounts));
+            }
         }
     }
 }
