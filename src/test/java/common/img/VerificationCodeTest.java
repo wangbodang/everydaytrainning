713e1bc93fcbef5a349e53a7c0f43b9d3baf0b3a
wangbodang <870719738@qq.com>
2019-02-18 17:21:37 +0800

    生成验证码 todo

--- src/test/java/common/img/VerificationCodeTest.java
+++ src/test/java/common/img/VerificationCodeTest.java
@@ -2,7 +2,12 @@ package common.img;
 
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class VerificationCodeTest {
 
@@ -10,7 +15,7 @@ public class VerificationCodeTest {
      * 生成随机验证码
      */
     @Test
-    public void testVerificationCode(){
+    public void testVerificationCode() throws IOException {
         double r = Math.random();
         int rInt = (int)(r*1000);
         System.out.println("--->rInt:"+rInt);
@@ -18,7 +23,13 @@ public class VerificationCodeTest {
         String fileName = rInt+".jpg";
         String filePath = "D:"+ File.separator+"TEST";
         File destFile = new File(filePath+File.separator+fileName);
+        FileOutputStream fos = new FileOutputStream(destFile);
+
+        //生成图片
+        BufferedImage bufferedImage = new BufferedImage(200, 40, BufferedImage.TYPE_4BYTE_ABGR_PRE);
 
 
+
+        ImageIO.write(bufferedImage, "jpg", fos);
     }
 }
