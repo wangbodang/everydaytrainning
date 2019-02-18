1402086ff3d8a563215b946a066a368f00ec878f
wangbodang <870719738@qq.com>
2019-02-18 16:03:05 +0800

    java的base64加密

--- src/test/java/common/apache_commons/CommonsCodecTest.java
+++ src/test/java/common/apache_commons/CommonsCodecTest.java
@@ -4,9 +4,13 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.net.URLCodec;
 import org.junit.Test;
+import sun.misc.BASE64Encoder;
 
 public class CommonsCodecTest {
 
+    /**
+     * 使用apache工具 以Base64加密
+     */
     @Test
     public void testBase64(){
         System.out.println("==============Base64================");
@@ -15,6 +19,23 @@ public class CommonsCodecTest {
         String encode = base64.encodeAsString(data);
         System.out.println(encode);
         System.out.println(new String(base64.decode(encode)));
+    }
+
+    /**
+     * 用java的 base64加密
+     */
+    @Test
+    public void testJavaBase64(){
+        System.out.println("==============Base64================");
+        byte[] data = "imooc".getBytes();
+
+        BASE64Encoder encoder = new BASE64Encoder();
+        String r1 = encoder.encode(data);
+        System.out.println(r1);
+
+        Base64 base64 = new Base64();
+        String r2 = base64.encodeAsString(data);
+        System.out.println(r2);
     }
 
     @Test
