398db0db66c4f614dd714bd9b84489204bb2907a
wangbodang <870719738@qq.com>
2019-03-04 09:31:24 +0800

    输入输出流的原始读写

--- src/test/java/common/stream/InputStreamTest.java
+++ src/test/java/common/stream/InputStreamTest.java
@@ -3,8 +3,7 @@ package common.stream;
 import org.apache.ibatis.io.Resources;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class InputStreamTest {
 
@@ -21,5 +20,48 @@ public class InputStreamTest {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * 测试输入输出流的原始读写方法
+     */
+    @Test
+    public void testInputStreamReadWrite(){
+        long startTime = System.currentTimeMillis();
+        System.out.println("Fook, Test");
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("excelTemp/excelDataType.xls");
+        if(is != null){
+            System.out.println("找到文件了");
+        }else{
+            System.out.println("找不到文件");
+        }
+        File outFile = new File("D:"+ File.separator+"TEST"+File.separator+"excelDataType.xls");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(outFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("没有找到输出文件对象");
+        }
+        byte[] buff = new byte[1024*1024];
+        int r = -1;
+        int i = 0;
+        try {
+            r = is.read(buff);
+            while (r != -1){
+                i++;
+                fos.write(buff);
+                r = is.read(buff);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("共有byte:"+i);
+        try {
+            fos.close();
+        } catch (IOException e) {
+            System.out.println("无法关闭文件");
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("共用时:"+(endTime-startTime)+"毫秒");
     }
 }
