167bbcbc36d26317657ca03198c3f6bfa0a30c60
wangbodang <870719738@qq.com>
2019-05-28 17:00:02 +0800

    stream operation

--- src/test/java/common/java8/StreamTest.java
+++ src/test/java/common/java8/StreamTest.java
@@ -3,12 +3,18 @@ package common.java8;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamTest {
+
+    @Test
+    public void test0(){
+
+    }
 
     @Test
     public void test01(){
@@ -74,5 +80,60 @@ public class StreamTest {
     private static String intCvtStr(Integer n){
         return n.toString()+"_xxx";
     }
+
+    @Test
+    public void test07(){
+        List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        // 获取空字符串的数量
+        long count = strings.stream().filter(string -> string.isEmpty()).count();
+        System.out.println(count);
+    }
+
+    @Test
+    public void test08(){
+        Random random = new Random();
+        random.ints().limit(10).sorted().forEach(System.out::println);
+    }
+
+    @Test
+    public void test09(){
+        List<String> strings = Arrays.asList("  ", "", "abc", "", "bc", "efg", "abcd","", "jkl");
+        // 获取空字符串的数量
+        long count = strings.parallelStream().filter(string -> string.isEmpty()).count();
+        System.out.println(count);
+    }
+
+    @Test
+    public void test10(){
+        List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
+
+        System.out.println("筛选列表: " + filtered);
+        String mergedString = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", "));
+        System.out.println("合并字符串: " + mergedString);
+
+        String result = strings.stream().collect(Collectors.joining("_"));
+        System.out.println(result);
+    }
+
+    @Test
+    public void test11(){
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+
+        IntSummaryStatistics stats = numbers.stream().mapToInt((x) -> x).summaryStatistics();
+
+        System.out.println("列表中最大的数 : " + stats.getMax());
+        System.out.println("列表中最小的数 : " + stats.getMin());
+        System.out.println("所有数之和 : " + stats.getSum());
+        System.out.println("平均数 : " + stats.getAverage());
+    }
+
+    @Test
+    public void test12(){
+        String[] strArr = new String[]{"182", "383", "384", "3483", "42813"};
+        IntSummaryStatistics statistics = Stream.of(strArr).mapToInt(str -> Integer.valueOf(str)).summaryStatistics();
+        System.out.println(statistics.getMax());
+    }
+
 
 }
