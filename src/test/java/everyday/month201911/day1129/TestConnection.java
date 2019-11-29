fe9a47ae029e67c0af0d727fb86dc2e88a43c6b5
wangbodang <870719738@qq.com>
2019-11-29 18:02:53 +0800

    测试connection的事务

---
+++ src/test/java/everyday/month201911/day1129/TestConnection.java
@@ -0,0 +1,62 @@
+package everyday.month201911.day1129;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TestConnection {
+
+    @Test
+    public void testConn() throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://127.0.0.1:3306/test?useSSL=false&serverTimezone=UTC";
+        String username = "root";
+        String password = "7241";
+        Class.forName(driver);
+        Connection conn = DriverManager.getConnection(url, username, password);
+
+        conn.setAutoCommit(false);
+
+        for(int i=0;i<10;i++){
+            insertAEmp(i, "wangbodang"+i, conn);
+        }
+
+        conn.close();
+    }
+
+    private void insertAEmp(int i, String name, Connection conn) {
+
+        String insertSql = "insert into employee (name, age, salary) values (?, ?, ?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(insertSql);
+            ps.setString(1, name);
+            ps.setInt(2, i*10);
+            ps.setBigDecimal(3, new BigDecimal(i*1000.1));
+            if(i==7){
+                "111".charAt(333);
+            }
+            ps.executeUpdate();
+
+            conn.commit();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception e){
+            System.out.println("执行出现异常 : "+e.getMessage());
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
